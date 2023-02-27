a239e8ffba1e329f3df6e4b413e8d433d7851132
kpwahn <kpwahnschaffe@gmail.com>
2023-02-27 14:54:16 +0000

    Playing with svgs

--- app/routes/city.tsx
+++ app/routes/city.tsx
@@ -1,11 +1,12 @@
 import PinkBuilding from "~/components/pink.building";
-import ShortOrange from "~/components/short.orange";
-import TallSkyBlue from "~/components/tall.sky.blue";
-import WaterTower from "~/components/water.tower";
+import PinkBuilding from "~/components/buildings/pink.building";
+import ShortOrange from "~/components/buildings/short.orange";
+import TallSkyBlue from "~/components/buildings/tall.sky.blue";
+import WaterTower from "~/components/buildings/water.tower";
 
 export default function Index() {
   return (
-    <main className="h-full bg-[#d5d6cf] p-4 text-xl md:p-8">
+    <main className="h-full bg-[#d5d6cf] p-4 md:p-8">
       <div className="-mx-4 h-2/3">
         <div className="relative h-full w-full border-b-2 border-[#403f3c]">
           <WaterTower />
---
+++ app/routes/index.jsx
@@ -0,0 +1,68 @@
+export default function Index() {
+  return (
+    <main className="p-4">
+      <svg width="430" height="1000" xmlns="http://www.w3.org/2000/svg" className="scale-50">
+        <rect width="100%" height="100%" fill="rgb(254 226 226)" />
+
+        <rect width="10" height="16" fill="black" x="290" y="30" />
+        <rect width="10" height="16" fill="black" x="305" y="30" />
+        
+        <rect width="10" height="16" fill="black" x="325" y="30" />
+        <rect width="10" height="16" fill="black" x="340" y="30" />
+
+        <rect width="10" height="16" fill="black" x="360" y="30" />
+        <rect width="10" height="16" fill="black" x="375" y="30" />
+        
+        <rect width="10" height="16" fill="black" x="395" y="30" />
+        <rect width="10" height="16" fill="black" x="410" y="30" />
+        
+        
+        
+
+
+
+
+        <rect width="10" height="16" fill="black" x="290" y="55" />
+        <rect width="10" height="16" fill="black" x="305" y="55" />
+        
+        <rect width="10" height="16" fill="black" x="325" y="55" />
+        <rect width="10" height="16" fill="black" x="340" y="55" />
+
+        <rect width="10" height="16" fill="black" x="360" y="55" />
+        <rect width="10" height="16" fill="black" x="375" y="55" />
+        
+        <rect width="10" height="16" fill="black" x="395" y="55" />
+        <rect width="10" height="16" fill="black" x="410" y="55" />
+        
+
+        <rect width="10" height="16" fill="black" x="290" y="85" />
+        <rect width="10" height="16" fill="black" x="305" y="85" />
+        
+        <rect width="10" height="16" fill="black" x="325" y="85" />
+        <rect width="10" height="16" fill="black" x="340" y="85" />
+
+        <rect width="10" height="16" fill="black" x="360" y="85" />
+        <rect width="10" height="16" fill="black" x="375" y="85" />
+        
+        <rect width="10" height="16" fill="black" x="395" y="85" />
+        <rect width="10" height="16" fill="black" x="410" y="85" />
+
+
+
+
+        <rect width="10" height="16" fill="black" x="290" y="110" />
+        <rect width="10" height="16" fill="black" x="305" y="110" />
+        
+        <rect width="10" height="16" fill="black" x="325" y="110" />
+        <rect width="10" height="16" fill="black" x="340" y="110" />
+
+        <rect width="10" height="16" fill="black" x="360" y="110" />
+        <rect width="10" height="16" fill="black" x="375" y="110" />
+        
+        <rect width="10" height="16" fill="black" x="395" y="110" />
+        <rect width="10" height="16" fill="black" x="410" y="110" />
+
+      </svg>
+    </main>
+  );
+}
