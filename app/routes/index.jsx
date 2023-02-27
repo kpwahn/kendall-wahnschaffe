54bc6a81e2ee9a175888e2ad11c30a51ca49e343
kpwahn <kpwahnschaffe@gmail.com>
2023-02-27 19:28:25 +0000

    Dabbling

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,68 +1,56 @@
+function PinkBuilding({className}) {
+  return (
+    <svg className={className} width="430" height="1000" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="rgb(254 226 226)" />
+        <rect width="2%" height="1.5%" fill="black" x="290" y="30" />
+        <rect width="2%" height="1.5%" fill="black" x="305" y="30" />
+        <rect width="2%" height="1.5%" fill="black" x="325" y="30" />
+        <rect width="2%" height="1.5%" fill="black" x="340" y="30" />
+        <rect width="2%" height="1.5%" fill="black" x="360" y="30" />
+        <rect width="2%" height="1.5%" fill="black" x="375" y="30" />        
+        <rect width="2%" height="1.5%" fill="black" x="395" y="30" />
+        <rect width="2%" height="1.5%" fill="black" x="410" y="30" />
+        <rect width="2%" height="1.5%" fill="black" x="290" y="55" />
+        <rect width="2%" height="1.5%" fill="black" x="305" y="55" />     
+        <rect width="2%" height="1.5%" fill="black" x="325" y="55" />
+        <rect width="2%" height="1.5%" fill="black" x="340" y="55" />
+        <rect width="2%" height="1.5%" fill="black" x="360" y="55" />
+        <rect width="2%" height="1.5%" fill="black" x="375" y="55" />
+        <rect width="2%" height="1.5%" fill="black" x="395" y="55" />
+        <rect width="2%" height="1.5%" fill="black" x="410" y="55" />
+        <rect width="2%" height="1.5%" fill="black" x="290" y="85" />
+        <rect width="2%" height="1.5%" fill="black" x="305" y="85" />
+        <rect width="2%" height="1.5%" fill="black" x="325" y="85" />
+        <rect width="2%" height="1.5%" fill="black" x="340" y="85" />
+        <rect width="2%" height="1.5%" fill="black" x="360" y="85" />
+        <rect width="2%" height="1.5%" fill="black" x="375" y="85" />
+        <rect width="2%" height="1.5%" fill="black" x="395" y="85" />
+        <rect width="2%" height="1.5%" fill="black" x="410" y="85" />
+        <rect width="2%" height="1.5%" fill="black" x="290" y="110" />
+        <rect width="2%" height="1.5%" fill="black" x="305" y="110" />
+        <rect width="2%" height="1.5%" fill="black" x="325" y="110" />
+        <rect width="2%" height="1.5%" fill="black" x="340" y="110" />
+        <rect width="2%" height="1.5%" fill="black" x="360" y="110" />
+        <rect width="2%" height="1.5%" fill="black" x="375" y="110" />
+        <rect width="2%" height="1.5%" fill="black" x="395" y="110" />
+        <rect width="2%" height="1.5%" fill="black" x="410" y="110" />
+    </svg>
+  )
+}
+
+function OrangeBuilding({className}) {
+  return (
+    <svg className={className} width="200" height="250" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="rgb(253 230 138)" />
+    </svg>
+  )
+}
+
 export default function Index() {
   return (
-    <main className="p-4">
-      <svg width="430" height="1000" xmlns="http://www.w3.org/2000/svg" className="scale-50">
-        <rect width="100%" height="100%" fill="rgb(254 226 226)" />
-
-        <rect width="10" height="16" fill="black" x="290" y="30" />
-        <rect width="10" height="16" fill="black" x="305" y="30" />
-        
-        <rect width="10" height="16" fill="black" x="325" y="30" />
-        <rect width="10" height="16" fill="black" x="340" y="30" />
-
-        <rect width="10" height="16" fill="black" x="360" y="30" />
-        <rect width="10" height="16" fill="black" x="375" y="30" />
-        
-        <rect width="10" height="16" fill="black" x="395" y="30" />
-        <rect width="10" height="16" fill="black" x="410" y="30" />
-        
-        
-        
-
-
-
-
-        <rect width="10" height="16" fill="black" x="290" y="55" />
-        <rect width="10" height="16" fill="black" x="305" y="55" />
-        
-        <rect width="10" height="16" fill="black" x="325" y="55" />
-        <rect width="10" height="16" fill="black" x="340" y="55" />
-
-        <rect width="10" height="16" fill="black" x="360" y="55" />
-        <rect width="10" height="16" fill="black" x="375" y="55" />
-        
-        <rect width="10" height="16" fill="black" x="395" y="55" />
-        <rect width="10" height="16" fill="black" x="410" y="55" />
-        
-
-        <rect width="10" height="16" fill="black" x="290" y="85" />
-        <rect width="10" height="16" fill="black" x="305" y="85" />
-        
-        <rect width="10" height="16" fill="black" x="325" y="85" />
-        <rect width="10" height="16" fill="black" x="340" y="85" />
-
-        <rect width="10" height="16" fill="black" x="360" y="85" />
-        <rect width="10" height="16" fill="black" x="375" y="85" />
-        
-        <rect width="10" height="16" fill="black" x="395" y="85" />
-        <rect width="10" height="16" fill="black" x="410" y="85" />
-
-
-
-
-        <rect width="10" height="16" fill="black" x="290" y="110" />
-        <rect width="10" height="16" fill="black" x="305" y="110" />
-        
-        <rect width="10" height="16" fill="black" x="325" y="110" />
-        <rect width="10" height="16" fill="black" x="340" y="110" />
-
-        <rect width="10" height="16" fill="black" x="360" y="110" />
-        <rect width="10" height="16" fill="black" x="375" y="110" />
-        
-        <rect width="10" height="16" fill="black" x="395" y="110" />
-        <rect width="10" height="16" fill="black" x="410" y="110" />
-
-      </svg>
+    <main className="h-full relative">
+      <PinkBuilding className="absolute bottom-0" />
+      <OrangeBuilding className="absolute bottom-0 right-0" />
     </main>
   );
 }
